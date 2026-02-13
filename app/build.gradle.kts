e5e2a7efc6a4e2c01bc8e6d2d496a01c48c4ce40
“SandipARP” <sandip@aresourcepool.com>
2026-02-13 17:55:38 +0530

    adding compatible version for justtip device

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.aresourcepool.discoverlauncher"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.aresourcepool.discoverlauncher"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 26
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
